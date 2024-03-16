9261407b15ccafcc2b2637ece8cc076cfa0a244c
maaax7 <maxtuka@gmail.com>
2024-03-16 13:32:43 +0000

    First commit

---
+++ Hackaton_g12/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Hackaton_g12.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
