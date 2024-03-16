808b70c6bde9801bfa47b6dec1cc3ba583348d2e
maaax7 <maxtuka@gmail.com>
2024-03-16 17:31:27 +0000

    Front end side

--- Hackaton_g12/Controllers/HomeController.cs
+++ Hackaton_g12/Controllers/HomeController.cs
@@ -1,21 +1,15 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Hackaton_g12.Models;
 
 namespace Hackaton_g12.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
+    private readonly List<UploadFile>? _files;
+    
     public IActionResult Index()
     {
-        return View();
+        return View(_files);
     }
 
     public IActionResult Privacy()
@@ -23,10 +17,56 @@ public class HomeController : Controller
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpPost]
+    public IActionResult UploadList(List<IFormFile> files)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        try
+        {
+            List<UploadFile> list = new List<UploadFile>();
+
+            foreach (var file in files)
+            {
+                string fileName = Path.GetFileName(file.FileName);
+                string description = $"File: {fileName}";
+                string status = "P"; 
+
+                list.Add(new UploadFile { Description = description, Status = status, ProcessDate = DateTime.Now });
+            }
+
+            return View(list);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"n error occurred while processing the upload: {ex.Message}");
+            return StatusCode(500, "An error occurred while processing the upload.");
+        }
+    }
+
+    public async Task<IActionResult> Download(int id)
+    {
+        UploadFile? file = GetFileById(id);  //teste
+
+        if (file == null)
+        {
+            return NotFound(); 
+        }
+
+        string? fileName = null;//file.FileName; 
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), "CaminhoParaOsArquivos", fileName!);
+
+        var memory = new MemoryStream();
+        using (var stream = new FileStream(filePath, FileMode.Open))
+        {
+            await stream.CopyToAsync(memory);
+        }
+        memory.Position = 0;
+
+        return File(memory, "application/octet-stream", Path.GetFileName(fileName));
+    }
+
+    private UploadFile? GetFileById(int id)
+    {
+        return null;
     }
 }
 
---
+++ Hackaton_g12/Models/UploadFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Hackaton_g12.Models;
+
+public class UploadFile
+{
+	public int Id { get; set; }
+	public string? Description { get; set; }
+	public string? Status { get; set; }
+	public DateTime? ProcessDate { get; set; }
+}
+
